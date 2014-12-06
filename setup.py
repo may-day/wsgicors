995dc84515f7b8699c887546b6c539b7d8108972
Norman Krämer <kraemer.norman@gmail.com>
2014-12-06 22:44:07 +0100

    bump version for pypi release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 import sys, os
 
-version = '0.3'
+version = '0.4'
 
 here = os.path.abspath(os.path.dirname(__file__))
 README = open(os.path.join(here, 'README.rst')).read()
